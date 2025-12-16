9f3274cc5bd60a7eb15eeba87b0f0dfd949649a5
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-16 19:46:14 -0300

    Refactor: organize UserController and UserModel into proper layers

--- src/main/java/dev/java10x/Usuarios/Controller/UserController.java
+++ src/main/java/dev/java10x/Usuarios/Controller/UserController.java
@@ -1,4 +1,4 @@
-package dev.java10x;
+package dev.java10x.Usuarios.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
