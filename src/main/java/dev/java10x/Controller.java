8b9f1963ae68f97779aafae2877dcb2b72fe2004
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-16 08:41:56 -0300

    Criado a controller

---
+++ src/main/java/dev/java10x/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String boasVinda(){
+        return "Bem-vindo ao sistema de cadastro de usuários!";
+    }
+
+}
