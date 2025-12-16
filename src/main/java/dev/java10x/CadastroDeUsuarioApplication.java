d79db2f115704cf5dc7a96bf7dc2294d04145dae
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-16 07:27:37 -0300

    Primeiro commit - API de cadastro de usuários com Spring Boot

---
+++ src/main/java/dev/java10x/CadastroDeUsuarioApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeUsuarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroDeUsuarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/java10x/CadastroDeUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeUsuarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
