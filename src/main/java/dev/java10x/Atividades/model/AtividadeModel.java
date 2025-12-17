89b1376f8c2e807ac163ae03f56507bf5d527969
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-17 09:21:11 -0300

    feat: adiciona anotação @ToString nos modelos UserModel e AtividadeModel

--- src/main/java/dev/java10x/Atividades/model/AtividadeModel.java
+++ src/main/java/dev/java10x/Atividades/model/AtividadeModel.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class AtividadeModel {
 
     @Id
--- src/main/java/dev/java10x/Usuarios/UserModel.java
+++ src/main/java/dev/java10x/Usuarios/UserModel.java
@@ -15,6 +15,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class UserModel {
 
     @Id
