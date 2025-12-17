8705ad09df537a83d29dbe73e498ec47cd5f00a5
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-17 08:58:12 -0300

    feat: adiciona dependência do Lombok aos modelos AtividadeModel e UserModel

--- src/main/java/dev/java10x/Atividades/model/AtividadeModel.java
+++ src/main/java/dev/java10x/Atividades/model/AtividadeModel.java
@@ -2,11 +2,19 @@ package dev.java10x.Atividades.model;
 
 import dev.java10x.Usuarios.UserModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_atividade_registration")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AtividadeModel {
 
     @Id
@@ -23,47 +31,5 @@ public class AtividadeModel {
     //uma atividade pode ter muitos usuários associados a ela
     @OneToMany(mappedBy = "atividade",cascade = CascadeType.ALL) //Relacionamento um-para-muitos com AtividadeModel
     List<UserModel> user; // Lista de usuários associados a esta atividade
-
-    protected AtividadeModel() {
-    }
-
-    public AtividadeModel(String name, String description, String type) {
-        this.name = name;
-        this.description = description;
-        this.type = type;
-    }
-
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
+    //mappedBy mapeia o atributo "atividade" na classe UserModel
 }
--- src/main/java/dev/java10x/Usuarios/UserModel.java
+++ src/main/java/dev/java10x/Usuarios/UserModel.java
@@ -5,11 +5,19 @@ package dev.java10x.Usuarios;
 
 import dev.java10x.Atividades.model.AtividadeModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity //Anotação que indica que esta classe é uma entidade JPA
 @Table(name = "tb_user_registration") //Anotação que define o nome da tabela no banco de dados
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserModel {
 
     @Id
@@ -22,57 +30,7 @@ public class UserModel {
 
     // Muitos usuários podem estar associados a uma única atividade
     @ManyToOne
-    @JoinColumn(name = "atividade_registration_id") //chave estrangeira para a tabela de atividades
+    @JoinColumn(name = "atividades_id") //Foreing Key ou chave estrangeira para a tabela de atividades
     private AtividadeModel atividade;
 
-    protected UserModel() {
-
-    }
-
-    public UserModel(String name, String department, String position, String technicalSkills) {
-        this.name = name;
-        this.department = department;
-        this.position = position;
-        this.technicalSkills = technicalSkills;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public String getTechnicalSkills() {
-        return technicalSkills;
-    }
-
-    public void setTechnicalSkills(String technicalSkills) {
-        this.technicalSkills = technicalSkills;
-    }
 }
