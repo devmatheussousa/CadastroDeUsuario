36483bb1bcd953fc975600055371a1bedda84e4a
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-18 08:25:59 -0300

    feat: cria a interface AtividadeRepository

---
+++ src/main/java/dev/java10x/Atividades/AtividadeRepository.java
@@ -0,0 +1,8 @@
+package dev.java10x.Atividades;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtividadeRepository extends JpaRepository<AtividadeModel, Long> {
+
+
+}
