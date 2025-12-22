845c3755e03cd59d593859459013be9ef464bd59
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-22 10:05:20 -0300

    feat: Cria injecao de dependencia

--- src/main/java/dev/java10x/Usuarios/UserService.java
+++ src/main/java/dev/java10x/Usuarios/UserService.java
@@ -1,4 +1,25 @@
 package dev.java10x.Usuarios;
 
+import lombok.RequiredArgsConstructor;
+
+//@RequiredArgsConstructor
 public class UserService {
+
+    //Ultilizar injecao de pedencia via construtor quando nao tiver final
+    //se tiver final ultiliza @RequidArgsConstructor
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+
+
+
+
+
+
+
+
 }
+
