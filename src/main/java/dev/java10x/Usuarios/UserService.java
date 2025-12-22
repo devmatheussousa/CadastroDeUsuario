50411fdb751786be2047d99d230f88a927726acb
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-22 10:07:41 -0300

    fix: adicionando anotacao @Service no UserService

--- src/main/java/dev/java10x/Usuarios/UserService.java
+++ src/main/java/dev/java10x/Usuarios/UserService.java
@@ -1,8 +1,10 @@
 package dev.java10x.Usuarios;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 //@RequiredArgsConstructor
+@Service
 public class UserService {
 
     //Ultilizar injecao de pedencia via construtor quando nao tiver final
