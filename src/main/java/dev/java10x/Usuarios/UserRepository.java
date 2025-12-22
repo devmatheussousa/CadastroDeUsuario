d61d8046386f2b903eaf1ffde7356f26d7df1f4f
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-22 10:18:09 -0300

    refactor: refatora UserRepository para interface JpaRepository

--- src/main/java/dev/java10x/Usuarios/UserRepository.java
+++ src/main/java/dev/java10x/Usuarios/UserRepository.java
@@ -1,7 +1,8 @@
 package dev.java10x.Usuarios;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<UserModel, Long> {
 }
