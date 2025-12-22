20fd0d47b6acc7aa5466f8ca82f4ca639d06fb1f
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-22 11:19:55 -0300

    refactor: modificar o end point para retorna a lista de atividades

--- src/main/java/dev/java10x/Atividades/AtividadesController.java
+++ src/main/java/dev/java10x/Atividades/AtividadesController.java
@@ -1,16 +1,22 @@
 package dev.java10x.Atividades;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 // Localhost:8080 -> estamos querendo criar rotas pro meu servidor
 @RestController // Isso é uma classe de controller, que vai receber requisições do usuario e devolver respostas
 @RequestMapping("/atividades") // Isso é a rota que o usuario vai acessar para fazer requisições
 // ele vai mapear as requisições para os metodos da classe
+@RequiredArgsConstructor
 public class AtividadesController {
 
+    private final AtividadeService atividadeService;
+
     @GetMapping
-    public String listarAtividades(){
-        return "Atividades listadas";
+    public List<AtividadeModel> listarAtividades(){
+        return atividadeService.listarTodasAtividades();
     }
 
     //localhost/8080/atividades/criar -> metodo POST
