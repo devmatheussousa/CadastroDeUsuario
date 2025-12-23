cb01c8fdfec7f0f39f6ccd4cea399ebf16551a26
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-23 10:23:01 -0300

    Chore: inclui a versao do DTO na entidade e mapeia manualmente UserMapper os caminhos

--- src/main/java/dev/java10x/Usuarios/UserMapper.java
+++ src/main/java/dev/java10x/Usuarios/UserMapper.java
@@ -1,4 +1,35 @@
 package dev.java10x.Usuarios;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+
+    //caminho
+    public UserModel map(UserDTO userDTO){
+        UserModel userModel = new UserModel();
+        userModel.setId(userDTO.getId());
+        userModel.setName(userDTO.getName());
+        userModel.setEmail(userDTO.getEmail());
+        userModel.setDepartment(userDTO.getDepartment());
+        userModel.setPosition(userDTO.getPosition());
+        userModel.setTechnicalSkills(userDTO.getTechnicalSkills());
+        userModel.setLevel(userDTO.getLevel());
+        userModel.setAtividade(userDTO.getAtividade());
+        return userModel;
+    }
+
+    //caminho inverso
+    public UserDTO map(UserModel userModel){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(userModel.getId());
+        userDTO.setName(userModel.getName());
+        userDTO.setEmail(userModel.getEmail());
+        userDTO.setDepartment(userModel.getDepartment());
+        userDTO.setPosition(userModel.getPosition());
+        userDTO.setTechnicalSkills(userModel.getTechnicalSkills());
+        userDTO.setLevel(userModel.getLevel());
+        userDTO.setAtividade(userModel.getAtividade());
+        return userDTO;
+    }
 }
