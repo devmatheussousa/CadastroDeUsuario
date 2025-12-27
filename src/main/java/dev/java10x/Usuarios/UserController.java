77eb0dd0b68ea94c260bf60f580b8d934b18a1ff
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-27 14:59:14 -0300

    fix: Adicionando respostas de atualizar para os usuarios

--- src/main/java/dev/java10x/Usuarios/UserController.java
+++ src/main/java/dev/java10x/Usuarios/UserController.java
@@ -1,6 +1,8 @@
 package dev.java10x.Usuarios;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,7 +12,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
-    //Mock de rota de boas vindas e retorno de mensagem
+    //Mock de rota de boas-vindas e retorno de mensagem
 
     private final UserService userService;
 
@@ -20,40 +22,49 @@ public class UserController {
 
     //adicionar Usuario (POST - Create)
     @PostMapping("/criar")
-    //O @RequestBody e usado quando eu quero que o usuario mande para min algo que vai compor o corpo da requisição (JSON)
+    //O @RequestBody e usado quando eu quero que o utilizador mande para min algo que vai compor o corpo da requisição (JSON)
     //@RequestBody - para pegar o valor do corpo da requisição (JSON)
-    //vamos pegar esse JSON serializado e transformar em um objeto UserModel e salvar no banco de dados
-    public UserDTO criarUsuario(@RequestBody UserDTO userDTO){
-        return userService.criarNovoUsuario(userDTO);
+    //vamos pegar esse JSON serializado e transformar num objeto UserModel e salvar no banco de dados
+    public ResponseEntity<String> criarUsuario(@RequestBody UserDTO userDTO) {
+        UserDTO novoUser = userService.criarNovoUsuario(userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Usuario criado com sucesso: " + novoUser.getName() + " com (id): " + novoUser.getId());
     }
 
     //Mostrar todos os Usuarios (GET - Read)
     @GetMapping("/todos")
-    public List<UserDTO> mostrarUsuarios(){
-        return userService.listarTodosUsuarios();
+    public ResponseEntity<List<UserDTO>> mostrarUsuarios() {
+        List<UserDTO> usuarios = userService.listarTodosUsuarios();
+        return ResponseEntity.status(HttpStatus.OK).body(usuarios);
     }
 
     //Mostrar usuario por id (GET - Read)
-    //@PathVariable - para pegar o valor da variável de caminho, quando eu quero que o usuario mande para min algo que vai compor a rota (URL)
+    //@PathVariable - para pegar o valor da variável de caminho, quando eu quero que o usuário mande para min algo que vai compor a rota (URL)
     //transforma o meu Long id como parte da minha rota ou URI {id} <- @PathVariable
     @GetMapping("/todos/{id}")
-    public UserDTO buscarUsuarioPorId(@PathVariable Long id){
-        return userService.listarPorId(id);
+    public ResponseEntity<UserDTO> buscarUsuarioPorId(@PathVariable Long id) {
+        UserDTO buscarPorId = userService.listarPorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(buscarPorId);
     }
 
     //Alterar dados do Usuario (PUT - Update)
     //Passa os dois argumentos, o id para buscar o usuario e o userAtualizado para atualizar os dados do usuario
-    //O put vai atulizar toda a estrutura do usuario, ou seja, se eu passar apenas um campo ele vai atualizar apenas aquele campo vai ser atualizado
-    //e se eu passar todos os campos ele vai atualizar todos os campos caso eu nao passe todos os campo os campo que eu nao passar ele vai ficar com null
+    //O put vai atualizar toda a estrutura do usuário, ou seja, se eu passar apenas um campo ele vai atualizar apenas aquele campo vai ser atualizado
+    //e se eu passar todos os campos ele vai atualizar todos os campos caso eu não passe todos os campo os campo que eu não passar ele vai ficar com null
     @PutMapping("/atualizar/{id}")
-    public UserDTO atualizarUsuario(@PathVariable Long id, @RequestBody UserDTO userAtualizado){
-        return userService.atualizarUsuario(id, userAtualizado);
+    public ResponseEntity<UserDTO> atualizarUsuario(@PathVariable Long id, @RequestBody UserDTO userAtualizado) {
+        UserDTO atualizarUser = userService.atualizarUsuario(id, userAtualizado);
+        return ResponseEntity.status(HttpStatus.OK).body(atualizarUser);
     }
 
     //Deletar Usuario (DELETE - Delete)
     @DeleteMapping("/deletar/{id}")
-    public void deletarUsuarioPorId(@PathVariable Long id){
-        userService.deletarUsuarioPorId(id);
+    public ResponseEntity<String> deletarUsuarioPorId(@PathVariable Long id) {
+        if (userService.listarPorId(id) != null) {
+            userService.deletarUsuarioPorId(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Usuario com id: " + id + " deletado com sucesso!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Erro usuario com id: " + id + " não foi encontrado para deletar!");
+        }
     }
 
 }
